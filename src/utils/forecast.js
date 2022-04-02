b8e020a96512182bdce9244b3c0a95061092b099
dterng928 <bhatarasiri_cud@hotmail.com>
2022-04-02 22:17:53 +0700

    Remove console.log add nodemon to devDependencies

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,6 @@ const forecast = (latitude, longitude, callback) => {
         }else if(body.error){
             callback('Unable to find location', undefined)
         }else{
-            console.log(body.current);
             callback(undefined, body.current.weather_descriptions[0] + '. It is currently '+ body.current.temperature + ' degrees out. It feels like '+ body.current.feelslike +' degrees out. The humidity is ' + body.current.humidity + '%.');
         }
     });
